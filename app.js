22989e91f929ed492f848a806d8ae1149b1dafb7
saadm2002 <72601361+saadm2002@users.noreply.github.com>
2020-11-05 22:51:30 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ document.querySelector('#create').addEventListener('click', function(){
 
     let allValidations = validateTaskForm(inputName, inputDescription, inputAssignedTo, inputDueDate, inputStatus);
     if(allValidations == true){
-        createTask(inputName, inputDescription, inputAssignedTo, inputDueDate, inputStatus, myTaskManager.Tasks);
+        createTask(inputName, inputDescription, inputAssignedTo, inputDueDate, inputStatus, myTaskManager.tasks);
         let taskIndex = myTaskManager.tasks.length-1;
         console.log(myTaskManager.tasks[taskIndex]);
         myTaskManager.addTask(myTaskManager.tasks[taskIndex])
