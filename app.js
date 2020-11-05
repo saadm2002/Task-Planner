4e8eaac3efbd29944c36adb82c483e25cc3817c5
saadm2002 <72601361+saadm2002@users.noreply.github.com>
2020-11-05 19:32:43 +0000

    Update app.js
    task 5 done

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ document.querySelector('#create').addEventListener('click', function(){
     let allValidations = validateTaskForm(inputName, inputDescription, inputAssignedTo, inputDueDate, inputStatus);
     if(allValidations == true){
         createTask(inputName, inputDescription, inputAssignedTo, inputDueDate, inputStatus, taskArray);
+        let taskIndex = myTaskManager.tasks.length-1;
+        console.log(myTaskManager.tasks[taskIndex]);
+        myTaskManager.addTask(myTaskManager.tasks[taskIndex])
     }
-    console.log(taskArray)
 })
 
 // Validate Inputs
@@ -28,7 +30,7 @@ function validateTaskForm(inputName, inputDescription, inputAssignedTo, inputDue
 // Store Tasks Into An Array
 
 function createTask(inputName, inputDescription, inputAssignedTo, inputDueDate, inputStatus, taskArray) {
-    taskArray.push({
+    myTaskManager.tasks.push({
         "Name": inputName,
         "Description": inputDescription,
         "AssignedTo": inputAssignedTo,
@@ -36,11 +38,11 @@ function createTask(inputName, inputDescription, inputAssignedTo, inputDueDate, 
         "Status": inputStatus,
         "TaskID": `${taskArray.length < 1 ? 1: taskArray.length +1}`
     })
-    return taskArray;
+    return myTaskManager.tasks;
 }
 
 class TaskManager {
-    constructor(){
+    constructor(array){
         this.tasks = array;
     }
 
@@ -48,8 +50,34 @@ class TaskManager {
 
     }
 
-    addTask(){
+    addTask(task){
 
+    let HTML = `<div class="col-md-4" taskID="${task.TaskID}">
+                    <div class="card">
+                    <div class="card-header">Task</div>
+                    <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Name: ${task.Name}</li>
+                    <li class="list-group-item">Description: ${task.Description}</li>
+                    <li class="list-group-item">Assigned To: ${task.AssignedTo}</li>
+                    <li class="list-group-item">Due Date: ${task.DueDate}</li>
+                    <li class="list-group-item">Status: ${task.Status}</li>
+                    </ul>
+                    </div>
+                    </div>`
+    
+    let HTMLRow = document.querySelector('#card');
+    HTMLRow.innerHTML += HTML;
+
+    let listHTML = `<a href="#" class="list-group-item list-group-item-action" taskID="${task.TaskID}">
+    <div class="d-flex justify-content-between">
+      <h5 class="mb-1">Assigned To: ${task.AssignedTo}</h5>
+      <small>Due Date: ${task.DueDate}</small>
+    </div>
+    <small>Status: ${task.Status}</small>
+  </a>`
+
+    let listHTMLRow = document.querySelector('#taskList');
+    listHTMLRow.innerHTML += listHTML;
     }
 
     deleteTask(){
